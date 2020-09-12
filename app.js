9189be94bda1b2bf78c5bddcd68033f8743c9b09
James <jamescarclan@yahoo.com>
2020-09-12 17:06:52 +0200

    Iteration2

--- app.js
+++ app.js
@@ -1,50 +1,49 @@
 require("dotenv").config();
-
 const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 const express = require("express");
 const favicon = require("serve-favicon");
-const cookieParser = require("cookie-parser");
 const hbs = require("hbs");
 const mongoose = require("mongoose");
-//const createError = require('http-errors');
 const logger = require("morgan");
 const path = require("path");
-
 const app_name = require("./package.json").name;
 const debug = require("debug")(
   `${app_name}:${path.basename(__filename).split(".")[0]}`
 );
-
+const app = express();
 // require database configuration
 require("./configs/db.config");
-
-// Routers
-
-const app = express();
-
-// Express View engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "hbs");
-
 // Middleware Setup
 app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-
+// SESSION CONFIG
+const session = require("express-session");
+const MongoStore = require("connect-mongo")(session);
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    cookie: { maxAge: 60 * 60 * 1000 },
+    saveUninitialized: false,
+    resave: true,
+    store: new MongoStore({
+      mongooseConnection: mongoose.connection,
+      ttl: 60 * 60 * 10000,
+    }),
+  })
+);
 // Express View engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "hbs");
 app.use(express.static(path.join(__dirname, "public")));
 app.use(favicon(path.join(__dirname, "public", "images", "favicon.ico")));
-
 // default value for title local
 app.locals.title = "Express - Generated with IronGenerator";
-
-// Routers
 const index = require("./routes/index.routes");
-const authRouter = require("./routes/auth.routes");
-
 app.use("/", index);
-app.use("/", authRouter);
+const auth = require("./routes/auth.routes");
+app.use("/", auth);
+
 module.exports = app;
---
+++ hash.js
@@ -0,0 +1,4 @@
+const bcrypt = require("bcrypt");
+// const password = "1234";
+const salt = bcrypt.genSaltSync();
+const hash = bcrypt.hashSync(password, salt);
